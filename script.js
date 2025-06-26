404dc575070a67ee44910849b65665a9d14be513
sanjarabidjanov <sanjarabidjanov240@gmail.com>
2025-06-26 15:24:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let studentName = prompt('string type dagi malumot kiriting ')
+let studentAge = +prompt('number type dagi malumot kiriting ')
+let studentAgeB = prompt('booling type dagi malumot kiriting ')
+alert(studentAgeB + studentName);
+console.log('sen tentaksa: ' + studentName)
+console.log('sen tentakni yoshin: ' + studentAge)
+console.log(Boolean(studentAge))
